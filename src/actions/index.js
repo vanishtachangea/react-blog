186e2ebc14294d640feb733bb851adca2ac8118e
vanishta <vanishta.changea@gmail.com>
2020-06-21 13:41:45 +0200

    chain

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,9 +5,14 @@ export const fetchPostsAndUsers = ()=> async(dispatch, getState)=>{
  //console.log('befer posts');
  await dispatch(fetchPosts());
  //console.log(getState().posts);
- const userIds = _.uniq(_.map(getState().posts, 'userId'));
+/*  const userIds = _.uniq(_.map(getState().posts, 'userId'));
  console.log(userIds);
- userIds.forEach(id => dispatch(fetchUser(id)));
+ userIds.forEach(id => dispatch(fetchUser(id))); */
+ _.chain(getState().posts)
+ .map('userId')
+ .uniq()
+ .forEach(id => dispatch(fetchUser(id)))
+ .value();
 
 }
 export const fetchPosts =() => async dispatch=>{
